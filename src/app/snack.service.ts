880e1923eafce3b3b1597d33d6a97fcfef09a4aa
Absner <absrraniel@gmail.com>
2017-04-25 13:34:30 -0400

    configurando servicio  snack

--- src/app/snack.service.ts
+++ src/app/snack.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { MdSnackBar } from '@angular/material';
 
 @Injectable()
 export class SnackService {
 
-  constructor() { }
+  constructor(public snackBar: MdSnackBar) { }
+
+  /**
+   * showSnackBar
+   */
+  public showSnackBar(message: string) {
+    this.snackBar.open(message, undefined, {
+        duration: 4000,
+    });
+    return this.snackBar._openedSnackBarRef;
+  }
 
 }
